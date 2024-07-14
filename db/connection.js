d99b8beeeacd8a53aca8e4c13e573b3bd2730f59
bikash1376 <139378098+bikash1376@users.noreply.github.com>
2024-07-14 19:27:07 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,21 @@
 import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 let db = null;
 
-export const connect = async done => {
+export const connect = async (done) => {
   const url = process.env.MONGO_DB_URL;
-
-  const dbName = "myBlogDB";
+  const dbName = process.env.DB_NAME || "myBlogDB"; // Using environment variable for dbName
 
   try {
-    let data = await MongoClient.connect(url, { useNewUrlParser: true });
-    db = data.db(dbName);
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    db = client.db(dbName);
+    console.log("Database connection established");
     done();
   } catch (error) {
+    console.error("Database connection failed: ", error);
     done(error);
   }
 };
